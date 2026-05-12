a0e1c994ae8cbf9089f4a4a1f505afa8f2e86d6b
Attitude <developer_03@wrfront.com>
2026-05-13 01:47:57 +0700

    Add check path / empty redirect to portal

--- api/src/controllers/redirectController.js
+++ api/src/controllers/redirectController.js
@@ -1,25 +1,27 @@
-const Link = require('../models/Link');
-const RedirectLog = require('../models/RedirectLog');
+const Link = require("../models/Link");
+const RedirectLog = require("../models/RedirectLog");
 
 exports.redirect = async (req, res) => {
+  if (!req.params.code) return res.redirect("https://portal.blly.to");
+
   try {
     const link = await Link.findOneAndUpdate(
       { code: req.params.code, isActive: true },
       { $inc: { clickCount: 1 } },
-      { new: true }
+      { new: true },
     );
 
-    if (!link) return res.status(404).send('Link not found');
+    if (!link) return res.status(404).send("Link not found");
 
     await RedirectLog.create({
       link: link._id,
-      ip: req.headers['x-forwarded-for']?.split(',')[0] || req.ip,
-      userAgent: req.headers['user-agent'],
+      ip: req.headers["x-forwarded-for"]?.split(",")[0] || req.ip,
+      userAgent: req.headers["user-agent"],
       referer: req.headers.referer || null,
     });
 
     res.redirect(link.originalUrl);
   } catch (err) {
-    res.status(500).send('Server error');
+    res.status(500).send("Server error");
   }
 };
